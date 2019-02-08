f94f3425798d75dea31ffde73580b21ca3def0be
trusttri <alwaysjane15@gmail.com>
2019-02-08 14:09:23 -0500

    Now the index.html under templates/website will be rendered. Also added a (temporary) function that gathers tweets once a username is given

--- russian_troll_detector/website/views.py
+++ russian_troll_detector/website/views.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import json
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('website/index.html')
+    context = {
+                }
+
+    return HttpResponse(template.render(context, request))
+
+def get_tweets(request):
+    data = 'FAIL'
+    if request.is_ajax():
+        print('is ajax')
+        username = request.GET.get('username', 'None')
+        print(username)
+    else:
+        data = 'Not an ajax request'
+
+    json_data = json.dumps(data)
+    return HttpResponse(json_data, content_type='application/json')
